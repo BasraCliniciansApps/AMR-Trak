3331f05829361518b98b80ffa4fc607a5b6166ce
BasraCliniciansApps <github.saadalhamady@gmail.com>
2026-09-16 18:24:21 +0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -710,10 +710,13 @@ records = applyWardFilter(records, $('input[name="guided_ward"]:checked').val() 
     
     if (records.length === 0) {
         $('#guidedContainer').addClass('hidden');
+        $('#guidedPlaceholder').removeClass('hidden');
         return;
     }
 
+    $('#guidedPlaceholder').addClass('hidden');
     $('#guidedContainer').removeClass('hidden');
+
     let orgCounts = {};
     records.forEach(r => {
         let org = r['Selective organism'];
